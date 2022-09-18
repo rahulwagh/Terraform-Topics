b579bd25a10bca3ab8932ff53444bf522d5189f3
rahulwagh <rahul.wagh@jhooq.com>
2022-09-18 18:14:32 +0300

    terraform aws multiprofile

---
+++ terrafrom-aws-multiprofile/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+   region = "eu-central-1"
+   shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+}
+
+resource "aws_instance" "ec2_example" {
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
