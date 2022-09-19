64e4c23e3dd13f38497f3bce541f289e52da1a81
Rahul Wagh <rahul.wagh@jhooq.com>
2022-09-19 15:50:56 +0300

    terraform ssh key generation

---
+++ terraform-aws-ssh-key-generator/main.tf
@@ -0,0 +1,27 @@
+
+provider "aws" {
+  region = "eu-central-1"
+  shared_credentials_files = ["/Users/rwagh/.aws/credentials"]
+}
+
+resource "tls_private_key" "terrafrom_generated_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+
+  # Name of key : Write custom name of your key
+  key_name   = "aws_keys_pairs"
+
+  # Public Key : The public will be generated using the refernce of tls_private_key.terrafrom_generated_private_key
+  public_key = tls_private_key.terrafrom_generated_private_key.public_key_openssh
+
+  # Store private key :  Generate and save private key(aws_keys_pairs.pem) in currect directory
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '${tls_private_key.terrafrom_generated_private_key.private_key_pem}' > aws_keys_pairs.pem
+      chmod 400 aws_keys_pairs.pem
+    EOT
+  }
+}
