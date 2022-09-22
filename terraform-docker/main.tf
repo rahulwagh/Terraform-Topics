59cbc0ff13830b1b0ffca493ca615b2ceb1dbb1b
Rahul Wagh <rahul.wagh@jhooq.com>
2022-09-22 13:54:52 +0300

    added description

---
+++ terraform-docker/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.21.0"
+    }
+  }
+}
+
+provider "docker" {
+}
+
+# Pulls the image
+resource "docker_image" "node-js-app" {
+  name = "terrafromvm/node-web-app"
+  keep_locally = false
+}
+
+
+resource "docker_container" "foo" {
+  image = docker_image.node-js-app.latest
+  name  = "tutorial"
+  ports {
+    internal = 8080
+    external = 49160
+  }
+}
