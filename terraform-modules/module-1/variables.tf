1f10ecef6dd4d7504f3582c78d822a6789d2bf91
avaus-rwagh <118802103+avaus-rwagh@users.noreply.github.com>
2022-12-29 13:24:48 +0200

    Terraform module

--- terraform-modules/module-1/variables.tf
+++ terraform-modules/module-1/variables.tf
@@ -1,2 +1,10 @@
-variable "web_instance_type" {default = "t2.micro"}
-variable "ami_id" {default = "ami-0767046d1677be5a0"}
+variable "web_instance_type"{
+  type        = string
+  description = "Instance type of EC2"
+  default     = "t2.micro"
+}
+variable "ami_id" {
+  type        = string
+  description = "AMI ID of EC2"
+  default     = "ami-0767046d1677be5a0"
+}
--- terraform-modules/variables.tf
+++ terraform-modules/variables.tf
@@ -1,5 +1,25 @@
-variable "web_region" {default = "eu-central-1"}
-variable "web_instance_type" {default = "t2.micro"}
-variable "ami_id" {default = "ami-0767046d1677be5a0"}
-variable "access_key" {default = ""}
-variable "secret_key" {default = ""}
+variable "web_region" {
+  type        = string
+  description = "Region of Project"
+  default     = "eu-central-1"
+}
+variable "web_instance_type" {
+  type        = string
+  description = "Instance Type of EC2"
+  default     = "t2.micro"
+}
+variable "ami_id" {
+  type        = string
+  description = "EC2 Machine AMI id"
+  default     = "ami-0767046d1677be5a0"
+}
+variable "access_key" {
+  type        = string
+  description = "Access key of AWS"
+  default     = ""
+}
+variable "secret_key" {
+  type        = string
+  description = "Secret Access key of AWS"
+  default     = ""
+}
