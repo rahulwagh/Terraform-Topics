14eaea3b0f695549bf5a6d9548b76c699e85118d
rahulwagh <rahul.wagh@jhooq.com>
2022-12-30 21:47:57 +0200

    EC2 instance setup

--- terraform-setup-ec2/main.tf
+++ terraform-setup-ec2/main.tf
@@ -3,24 +3,16 @@ provider "aws" {
   shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
 }
 
-resource "aws_instance" "ec2_example" {
 
+resource "aws_instance" "example" {
   ami = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
-  key_name= "aws_key"
   vpc_security_group_ids = [aws_security_group.main.id]
-
-  provisioner "file" {
-    source      = "/home/rahul/Jhooq/keys/aws/test-file.txt"
-    destination = "/home/ubuntu/test-file.txt"
-  }
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("/Users/rahulwagh/.ssh/aws_ec2_terraform")
-    timeout     = "4m"
-  }
+  key_name= "aws_key"
+  /*user_data     = templatefile("${path.module}/user_data.tpl", {
+    package = "nginx"
+    command = "systemctl start nginx"
+  })*/
 }
 
 resource "aws_security_group" "main" {
@@ -61,10 +53,13 @@ resource "aws_security_group" "main" {
       to_port          = 443
     }
   ]
-
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbvRN/gvQBhFe+dE8p3Q865T/xTKgjqTjj56p1IIKbq8SDyOybE8ia0rMPcBLAKds+wjePIYpTtRxT9UsUbZJTgF+SGSG2dC6+ohCQpi6F3xM7ryL9fy3BNCT5aPrwbR862jcOIfv7R1xVfH8OS0WZa8DpVy5kTeutsuH5FMAmEgba4KhYLTzIdhM7UKJvNoUMRBaxAqIAThqH9Vt/iR1WpXgazoPw6dyPssa7ye6tUPRipmPTZukfpxcPlsqytXWlXm7R89xAY9OXkdPPVsrQA0XFQnY8aFb9XaZP8cm7EOVRdxMsA1DyWMVZOTjhBwCHfEIGoePAS3jFMqQjGWQd rahul@rahul-HP-ZBook-15-G2"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDwmgMHFJE7J4qepIzAZL3/yC6J0zsEAb/oHYL+WBBDNUjSH4TeHUnHVNe9b/pyPcub+O/HNvlGrzSxUp0xT0b3O7kkTtgBKiG8NaBbonj+c7byfOGER80DYxc5adlBltuIDd8StFe7OMzbYyUSr1mdxDTIWm/OoE39G/fu3hTqUGkykv072GAy8nMFejITRw9pf+53B9ziE5rsdOUH4uqBiQa/Ng/qKo7h9MtJGcloRATYiObXwAgrHtt3sDrtvkq2ZceT906/BJm1Twlm+BHlQecHV18Ak3bzm/6HzlsA/q+yORsoB+VxSUxvVy0nXTc1X8vJAD4KSYVL5DTrpisdnQAIcuqAbea+LMku2o4sdnrrIlUi8/8BXeVbI4TNNGd0+sWpCVcDEhb4gyA/XXTvloQyjTYrL4+am/9XEY6NGdsrPK74sjvtpUZPUrmzTJ/mJWG5ncGY88GAj+YZAsY5pnAqh2CkR2TUpglugldnWyrppbe2QyC9iQkgUGSkBTs="
+}
+
+output "fetched_info_from_aws" {
+  value = format("%s%s","ssh -i /Users/rahulwagh/.ssh/aws_ec2_terraform ubuntu@",aws_instance.example.public_dns)
 }
