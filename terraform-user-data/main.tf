eaa1fa088cd1642c0dbb19b53a6aa8baf54cfcb7
rahulwagh <rahul.wagh@jhooq.com>
2023-01-02 14:00:10 +0200

    user_data block

--- terraform-user-data/main.tf
+++ terraform-user-data/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.main.id]
   key_name= "aws_key"
-  //user_data     =  "${file("install_nginx.sh")}"
+  //user_data     =  "${file("install_apache.sh")}"
   user_data     = <<-EOF
                     #!/bin/bash
                     echo "Hello, World!" > hello.txt
