9089da8fdd88e722f52532a9e3e0cf6ab6ab9e89
avaus-rwagh <118802103+avaus-rwagh@users.noreply.github.com>
2023-01-12 07:50:04 +0200

    Terraform ansible project and gcp

---
+++ terraform-ansible-proj/gcp-cloud-infra.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.47.0"
+    }
+  }
+}
+provider "google" {
+  credentials = file("/Users/rahulwagh/.gcp/credentials.json")
+  project     = var.project_id
+  region  = "europe-west4"
+  zone    = "europe-west4-a"
+}
+
+resource "google_compute_instance" "terraform-managed-e2-small" {
+  name         = "terraform"
+  machine_type = "e2-small"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "rahulwagh:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDwmgMHFJE7J4qepIzAZL3/yC6J0zsEAb/oHYL+WBBDNUjSH4TeHUnHVNe9b/pyPcub+O/HNvlGrzSxUp0xT0b3O7kkTtgBKiG8NaBbonj+c7byfOGER80DYxc5adlBltuIDd8StFe7OMzbYyUSr1mdxDTIWm/OoE39G/fu3hTqUGkykv072GAy8nMFejITRw9pf+53B9ziE5rsdOUH4uqBiQa/Ng/qKo7h9MtJGcloRATYiObXwAgrHtt3sDrtvkq2ZceT906/BJm1Twlm+BHlQecHV18Ak3bzm/6HzlsA/q+yORsoB+VxSUxvVy0nXTc1X8vJAD4KSYVL5DTrpisdnQAIcuqAbea+LMku2o4sdnrrIlUi8/8BXeVbI4TNNGd0+sWpCVcDEhb4gyA/XXTvloQyjTYrL4+am/9XEY6NGdsrPK74sjvtpUZPUrmzTJ/mJWG5ncGY88GAj+YZAsY5pnAqh2CkR2TUpglugldnWyrppbe2QyC9iQkgUGSkBTs= rahulwagh@Rahuls-MacBook-Pro.local"
+  }
+}
+
+output "gce2-public-ip" {
+  value = format("%s%s","ssh -i /Users/rahulwagh/.ssh/aws_ec2_terraform rahulwagh@",google_compute_instance.terraform-managed-e2-small.network_interface.0.access_config.0.nat_ip)
+}
+
+
+
---
+++ terraform-ansible-proj/variables.tf
@@ -0,0 +1,4 @@
+variable "project_id" {
+  type        = string
+  description = "The Google Project ID"
+}
