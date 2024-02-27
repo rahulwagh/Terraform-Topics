8f5e09985ffea8b7174d5dc78b0c12aa58fe5813
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-02-27 09:04:01 +0100

    Update main.tf - typo fixes

--- aws-lambda/main.tf
+++ aws-lambda/main.tf
@@ -74,14 +74,14 @@ resource "aws_lambda_function" "terraform_lambda_func" {
 }
 
 
-output "teraform_aws_role_output" {
+output "terraform_aws_role_output" {
  value = aws_iam_role.lambda_role.name
 }
 
-output "teraform_aws_role_arn_output" {
+output "terraform_aws_role_arn_output" {
  value = aws_iam_role.lambda_role.arn
 }
 
-output "teraform_logging_arn_output" {
+output "terraform_logging_arn_output" {
  value = aws_iam_policy.iam_policy_for_lambda.arn
 }
